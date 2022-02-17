585c3ddb06e10bc7492b9761c9cc3a9b1f037f55
moreharsh <moreharsh2001@gmail.com>
2022-02-17 14:33:31 +0530

    carousel updated, links added

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,11 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-  el: ".swiper-pagination",
-  clickable: true,
-  },
+  slidesPerGroup: 3
 });
